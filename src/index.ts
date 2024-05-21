2342b02cb2a554cf1445a151693ed759613842d2
Mogamat Muneeb Davids <davimuneeb785@gmail.com>
2024-05-21 23:15:29 +0200

    wip: added vercel json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 9000;
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Express & TypeScript Server");
